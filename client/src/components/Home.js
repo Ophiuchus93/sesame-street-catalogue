a8d5d3b4b8f781469e222e4df6bfafbe21eca8c9
Scott (DC)  Christensen <schristensen10@gmail.com>
2020-01-15 17:16:59 -0700

    muppets

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Characters from './Muppets';
 import { Typography as Type, } from '@material-ui/core';
 
 const Home = () => (
+  <>
   <Type variant='h3' align='center'>Devise Auth App</Type>
+    <Characters />
+  </>
 );
 
 export default Home;
---
+++ client/src/components/Muppets.js
@@ -0,0 +1,14 @@
+import React, { useState, } from 'react'
+
+const Muppets = () => {
+  const [ name, setName, ] = useState();
+  const [ description, setDescription, ] = useState();
+
+  return (
+    <>
+      Test
+    </>
+  )
+};
+
+export default Muppets;
